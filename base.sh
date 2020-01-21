fa642f152f261723a504ad405e186737a660412a
Jonas Bushart <jonas@bushart.org>
2020-01-22 00:02:40 +0100

    Enable more customization how containers are spawned (#2)
    Introduce the PODMAN_RUN_ARGS variable to hold additional arguments for
    `podman run`. It can be set in the custom_base.sh file.

--- base.sh
+++ base.sh
@@ -1,7 +1,15 @@
 # shellcheck shell=bash disable=SC2034
 
+currentDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
 # Variables defined here will be availble to all the scripts.
 
 CONTAINER_ID="runner-$CUSTOM_ENV_CI_RUNNER_ID-project-$CUSTOM_ENV_CI_PROJECT_ID-concurrent-$CUSTOM_ENV_CI_CONCURRENT_PROJECT_ID-$CUSTOM_ENV_CI_JOB_ID"
 IMAGE="$CUSTOM_ENV_CI_JOB_IMAGE"
 CACHE_DIR="$(dirname "${BASH_SOURCE[0]}")/../_cache/runner-$CUSTOM_ENV_CI_RUNNER_ID-project-$CUSTOM_ENV_CI_PROJECT_ID-concurrent-$CUSTOM_ENV_CI_CONCURRENT_PROJECT_ID"
+
+# Execute customization code
+if [ -f "${currentDir}"/custom_base.sh ]; then
+    # shellcheck source=custom_base.sh disable=SC1091
+    source "${currentDir}"/custom_base.sh
+fi
