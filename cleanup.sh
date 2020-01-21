240c6adabba62b8ee7bd407ed5b6de6327f795a4
Jonas Bushart <jonas@bushart.org>
2020-01-22 00:20:37 +0100

    Try harder to cleanup stale podman resources

--- cleanup.sh
+++ cleanup.sh
@@ -11,6 +11,9 @@ echo "Deleting container $CONTAINER_ID"
 podman kill "$CONTAINER_ID"
 podman rm "$CONTAINER_ID"
 
+# Try to remove all old containers, images, networks, and volumes
+podman system prune --force --volumes
+
 # Delete leftover files in /tmp
 rm -r "$TMPDIR"
 
