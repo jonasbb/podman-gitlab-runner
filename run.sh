d25fa4f85b44599328229427542279782202fec6
Jonas Bushart <jonas@bushart.org>
2020-08-20 23:59:37 +0200

    Remove workaround for stdin problem
    Podman 1.9 fixed reading from stdin.
    
    Fixes #3

--- run.sh
+++ run.sh
@@ -4,12 +4,7 @@ currentDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 # shellcheck source=base.sh
 source "${currentDir}"/base.sh
 
-# Workaround for bug: https://github.com/containers/libpod/issues/4326
-# Based on: https://github.com/containers/libpod/issues/4326#issuecomment-572047595
-# podman exec cannot read from stdin
-# Therefore, we copy the file into the container and execute it from there
-CMD=$(cat "$1")
-if ! podman exec "$CONTAINER_ID" /bin/bash -c "$CMD"
+if ! podman exec "$CONTAINER_ID" /bin/bash < "$1"
 then
     # Exit using the variable, to make the build as failure in GitLab CI.
     exit "$BUILD_FAILURE_EXIT_CODE"
