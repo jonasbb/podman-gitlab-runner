9e0df8326fe1f65ef21e45aa2aeeef0f35b5de35
Jonas Bushart <jonas@bushart.org>
2020-09-07 17:33:14 +0200

    Fix run.sh
    Reading from stdin with `exec` only works when `-i` is also passed.

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ currentDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 # shellcheck source=base.sh
 source "${currentDir}"/base.sh
 
-if ! podman exec "$CONTAINER_ID" /bin/bash < "$1"
+if ! podman exec -i "$CONTAINER_ID" /bin/bash < "$1"
 then
     # Exit using the variable, to make the build as failure in GitLab CI.
     exit "$BUILD_FAILURE_EXIT_CODE"
